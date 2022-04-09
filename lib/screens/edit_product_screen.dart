c3c683d2ac1c6e51390e7b1b22ee0e4d830acff8
HASSAN <80027564+0hassan@users.noreply.github.com>
2022-04-10 01:48:33 +0500

    v1

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -10,6 +10,15 @@ class EditProductScreen extends StatefulWidget {
 
 class _EditProductScreenState extends State<EditProductScreen> {
   final _priceFocusNode = FocusNode();
+  final _imageurlcontroler = TextEditingController();
+
+  @override
+  void dispose() {
+    _priceFocusNode.dispose();
+    _imageurlcontroler.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -45,10 +54,46 @@ class _EditProductScreenState extends State<EditProductScreen> {
               decoration: const InputDecoration(
                 labelText: 'Description',
               ),
-              textInputAction: TextInputAction.next,
+              // textInputAction: TextInputAction.next,
               keyboardType: TextInputType.multiline,
               maxLines: 3,
             ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(
+                    top: 8,
+                    right: 10,
+                    left: 4,
+                  ),
+                  height: 100,
+                  width: 100,
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.grey,
+                      width: 1,
+                      style: BorderStyle.solid,
+                    ),
+                  ),
+                  child: _imageurlcontroler.text.isEmpty
+                      ? const Text('Énter a URL.')
+                      : Image.network(
+                          _imageurlcontroler.text,
+                          fit: BoxFit.cover,
+                        ),
+                ),
+                Expanded(
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                      labelText: 'Image URL',
+                    ),
+                    controller: _imageurlcontroler,
+                    textInputAction: TextInputAction.done,
+                  ),
+                ),
+              ],
+            )
           ],
         ),
       ),
